08343139db76ba775f96b3b374a667bebce9b0c5
Mauricio Escobar <mauricio@MacBook-Pro-1-de-Possumus.local>
2017-04-21 12:14:12 -0300

    Se agrego metodo POST y GET

---
+++ appIselin/RestApiManager.swift
@@ -0,0 +1,9 @@
+//
+//  RestApiManager.swift
+//  appIselin
+//
+//  Created by Mauricio Escobar on 20/4/17.
+//  Copyright © 2017 Mauricio Escobar. All rights reserved.
+//
+
+import Foundation
---
+++ appIselin/ViewTwo.swift
@@ -0,0 +1,9 @@
+//
+//  ViewTwo.swift
+//  appIselin
+//
+//  Created by Mauricio Escobar on 19/4/17.
+//  Copyright © 2017 Mauricio Escobar. All rights reserved.
+//
+
+import Foundation
